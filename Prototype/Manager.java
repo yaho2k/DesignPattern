05c389cc640204bc65fdffd886b34ce49a42fa5b
Jeehwan Park <yaho2k@gmail.com>
2021-09-10 08:04:40 +0900

    Prototype

---
+++ Prototype/Manager.java
@@ -0,0 +1,16 @@
+package Prototype;
+
+import java.util.HashMap;
+
+public class Manager {
+    private HashMap<String, Product> showcase = new HashMap<>();
+
+    public void register(String name, Product proto) {
+        showcase.put(name, proto);
+    }
+
+    public Product create(String protoname) {
+        Product p = (Product) showcase.get(protoname);
+        return p.createClone();
+    }
+}
