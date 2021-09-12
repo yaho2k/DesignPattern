feeb57d865178ad3c65961bf367a156c115fdd9d
Jeehwan Park <yaho2k@gmail.com>
2021-09-12 21:36:26 +0900

    Ploxy

---
+++ Proxy/Main.java
@@ -0,0 +1,11 @@
+package Proxy;
+
+public class Main {
+    public static void main(String[] args) {
+        Printable p = new PrinterProxy("Alice");
+        System.out.println("Name is " + p.getPrinterName() + ".");
+        p.setPrinterName("Bob");
+        System.out.println("Name is " + p.getPrinterName() + ".");
+        p.print("Hello, world.");
+    }
+}
