0de03a33182b861c37e241e3bd0239b6e54275b7
Jeehwan Park <yaho2k@gmail.com>
2021-09-12 12:22:30 +0900

    Facade

---
+++ Facade/Main.java
@@ -0,0 +1,9 @@
+package Facade;
+
+import Facade.pagemaker.PageMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("youngjin@youngjin.com", "welcome.html");
+    }
+}
